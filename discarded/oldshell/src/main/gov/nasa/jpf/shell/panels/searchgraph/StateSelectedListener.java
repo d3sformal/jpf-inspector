ad03bd2fbeeeb4d6311afb3898fe1a4045e40f1b
Petr Hudeček <petrhudecek2010@gmail.com>
2016-05-27 13:23:57 +0200

    remove old shell

--- discarded/oldshell/src/main/gov/nasa/jpf/shell/panels/searchgraph/StateSelectedListener.java
+++
@@ -1,7 +0,0 @@
-package gov.nasa.jpf.shell.panels.searchgraph;
-
-import java.util.Collection;
-
-public interface StateSelectedListener {
-	void stateSelected(NodeInfo n, Collection<NodeInfo> path);
-}
