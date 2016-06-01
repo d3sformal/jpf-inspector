c73ad6f5599b382e2b68e4f3925671ee19e0d976
Petr Hudeček <petrhudecek2010@gmail.com>
2016-06-01 15:13:41 +0200

    help

--- jpf-inspector/src/main/gov/nasa/jpf/inspector/client/commands/CmdHelp.java
+++ jpf-inspector/src/main/gov/nasa/jpf/inspector/client/commands/CmdHelp.java
@@ -23,18 +23,21 @@ public class CmdHelp extends ClientCommand {
     informationalCommands.add(new CommandHelpInfo("help", null, "Prints this message."));
     informationalCommands.add(new CommandHelpInfo("thread [id]", "ti", "Prints info about a thread.."));
     informationalCommands.add(new CommandHelpInfo("thread_pc [id]", "thpc", "Prints current instruction of a thread."));
+    informationalCommands.add(new CommandHelpInfo("print [expression]", null, "Evaluates an expression."));
     categories.put("Informational commands", informationalCommands);
 
     ArrayList<CommandHelpInfo> execution = new ArrayList<>();
     execution.add(new CommandHelpInfo("quit", "exit", "Closes the shell and terminates the process."));
     execution.add(new CommandHelpInfo("continue", "run, cont", "Starts or resumes execution."));
     execution.add(new CommandHelpInfo("break", null, "Suspends execution."));
+    execution.add(new CommandHelpInfo("set [target] = [value]", null, "Modifies a variable's value."));
     categories.put("Execution commands", execution);
 
     ArrayList<CommandHelpInfo> breakpoints = new ArrayList<>();
     breakpoints.add(new CommandHelpInfo("create breakpoint [properties...] [hit condition]", "cr bp", "Creates a new breakpoint."));
     breakpoints.add(new CommandHelpInfo("show breakpoint", "sw bp", "Prints all breakpoints."));
     breakpoints.add(new CommandHelpInfo("delete breakpoint [id]", "del bp", "Deletes the breakpoint."));
+    breakpoints.add(new CommandHelpInfo("assert [file]:[line] [condition]", null, "Creates a dynamic assertion."));
     categories.put("Breakpoint-related commands", breakpoints);
 
     ArrayList<CommandHelpInfo> recording = new ArrayList<>();
@@ -44,7 +47,19 @@ public class CmdHelp extends ClientCommand {
     recording.add(new CommandHelpInfo("record save [filename]", "rec save", "Saves the recording into a file."));
     categories.put("Recording commands", recording);
 
-    categories.put("Stepping-related commands", new ArrayList<>());
+    ArrayList<CommandHelpInfo> stepping = new ArrayList<>();
+    stepping.add(new CommandHelpInfo("step_instruction", "sins", "Executes one instruction."));
+    stepping.add(new CommandHelpInfo("step_over", "so", "Executes current line."));
+    stepping.add(new CommandHelpInfo("step_out", "sout", "Executes until end of method."));
+    stepping.add(new CommandHelpInfo("step_in", "si", "Executes one line or until a method is entered."));
+    stepping.add(new CommandHelpInfo("step_transition", "st", "Executes until next choice."));
+
+    stepping.add(new CommandHelpInfo("back_step_instruction", "bsins", "Undoes one instruction."));
+    stepping.add(new CommandHelpInfo("back_step_over", "bso", "Undoes current source line."));
+    stepping.add(new CommandHelpInfo("back_step_out", "bsout", "Undoes instructions until we return to caller."));
+    stepping.add(new CommandHelpInfo("back_step_in", "bsi", "Undoes this and previous line or until a method call is undone."));
+    stepping.add(new CommandHelpInfo("back_step_transition", "bst", "Undoes until a choice point is reached."));
+    categories.put("Stepping-related commands", stepping);
 
     outStream.println("You may use the following commands in this console:\n");
     for(Map.Entry<String, List<CommandHelpInfo>> entry : categories.entrySet()) {
