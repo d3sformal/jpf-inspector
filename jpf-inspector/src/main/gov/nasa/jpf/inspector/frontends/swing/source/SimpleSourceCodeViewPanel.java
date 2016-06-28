c6af0023546144d6006c8008800c5b9eda5e69c8
Petr Hudeček <petrhudecek2010@gmail.com>
2016-06-28 12:41:18 +0200

    source code view now works with highlighting and all that

--- jpf-inspector/src/main/gov/nasa/jpf/inspector/frontends/swing/source/SimpleSourceCodeViewPanel.java
+++ jpf-inspector/src/main/gov/nasa/jpf/inspector/frontends/swing/source/SimpleSourceCodeViewPanel.java
@@ -1,40 +1,36 @@
 package gov.nasa.jpf.inspector.frontends.swing.source;
 
+import gov.nasa.jpf.inspector.common.Constants;
 import gov.nasa.jpf.inspector.frontends.swing.AuxiliaryInspectorPanel;
 import gov.nasa.jpf.util.Source;
 import gov.nasa.jpf.vm.ClassInfo;
 import gov.nasa.jpf.vm.Instruction;
 import gov.nasa.jpf.vm.MethodInfo;
 import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
-import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
-import org.fife.ui.rsyntaxtextarea.folding.Fold;
-import org.fife.ui.rsyntaxtextarea.folding.FoldManager;
-import org.fife.ui.rtextarea.RTextScrollPane;
 
 import javax.swing.*;
-import javax.swing.text.BadLocationException;
 import java.awt.*;
 
 /**
  * The source code view panel allows the user to view the source code of the file where the
  * currently executed instruction resides.
+ *
+ * This is an older implementation that uses a simple {@link JEditorPane} and could function as a fallback
+ * in case {@link RSyntaxTextArea} somehow doesn't work.
+ *
+ * However, {@link RSyntaxTextArea} seems to work reliably so this panel could conceivably be deleted altogether.
  */
-public class SourceCodeViewPanel extends AuxiliaryInspectorPanel {
-  private static final long serialVersionUID = -332650333255593274L;
-  private static final java.awt.Color currentLineColor = new Color(193,247,213);
+@SuppressWarnings("unused")
+public class SimpleSourceCodeViewPanel extends AuxiliaryInspectorPanel {
   private final JLabel currentInformationLabel;
-  private final RSyntaxTextArea textArea;
+  private final JEditorPane sourceCodeTextArea;
 
-  public SourceCodeViewPanel() {
+  public SimpleSourceCodeViewPanel() {
     super("Source", null, "View the source code of the currently executed instruction.");
     setLayout(new BorderLayout());
-    textArea = new RSyntaxTextArea();
-    textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
-    textArea.setCodeFoldingEnabled(true);
-    textArea.setEnabled(false);
-    RTextScrollPane scrollPane = new RTextScrollPane(textArea);
-    scrollPane.setIconRowHeaderEnabled(true);
-    add(scrollPane);
+    sourceCodeTextArea = new JEditorPane();
+    sourceCodeTextArea.setFont(Constants.fontMonospaced);
+    add(new JScrollPane(sourceCodeTextArea), BorderLayout.CENTER);
     currentInformationLabel = new JLabel("Current instruction: ???, line ???, file ???.");
     add(currentInformationLabel, BorderLayout.SOUTH);
   }
@@ -67,6 +63,9 @@ public class SourceCodeViewPanel extends AuxiliaryInspectorPanel {
       int lineCount = source.getLineCount();
       for (int i = 1; i <= lineCount; i++) {
         String thisLine= source.getLine(i);
+        if (i == currentInstruction.getLineNumber()) {
+          thisLine += " <<< CURRENT LINE";
+        }
         sourceBuilder.append(thisLine).append("\n");
       }
       activeInstructionChanged(currentInstruction.getMnemonic(), sourceFileName, currentInstruction.getLineNumber(), sourceBuilder.toString());
@@ -78,37 +77,8 @@ public class SourceCodeViewPanel extends AuxiliaryInspectorPanel {
                                             (line >= 0 ? ", line " + line : "") +
                                             (filename != null ? ", file " + filename : "") +
                                             (sourcecode != null ? "": ", source unavailable"));
-    if (sourcecode != null && !textArea.getText().equals(sourcecode)) {
-      textArea.setText(sourcecode);
-    }
-    textArea.removeAllLineHighlights();
-    int currentLine = line - 1;
-
-    // Make sure the highlighted line is visible
-    FoldManager foldManager = textArea.getFoldManager();
-    for (int foldIndex = 0; foldIndex < foldManager.getFoldCount(); foldIndex++) {
-      Fold fold = foldManager.getFold(foldIndex);
-      if (fold.containsLine(currentLine)) {
-        unfoldRecursively(fold, currentLine);
-      }
-    }
-
-    // Highlight the line
-    try {
-      textArea.addLineHighlight(currentLine,currentLineColor);
-      textArea.setCaretPosition(textArea.getLineStartOffset(currentLine));
-    } catch (BadLocationException e) {
-      // Ignore.
-    }
-  }
-
-  private static void unfoldRecursively(Fold fold, int currentLine) {
-    fold.setCollapsed(false);
-    for (int i =0 ; i < fold.getChildCount(); i++) {
-      Fold childFold = fold.getChild(i);
-      if (childFold.containsLine(currentLine)) {
-        unfoldRecursively(childFold, currentLine);
-      }
+    if (sourcecode != null) {
+      sourceCodeTextArea.setText(sourcecode);
     }
   }
 }
