960bb64c49a5cae2331e020d4f0269e047a3eb98
Petr Hudeček <petrhudecek2010@gmail.com>
2016-07-26 14:38:00 +0200

    removing old jpf

--- core6/src/peers/gov/nasa/jpf/jvm/JPF_java_lang_Boolean.java
+++
@@ -1,9 +0,0 @@
-package gov.nasa.jpf.jvm;
-
-public class JPF_java_lang_Boolean {
-  // <2do> at this point we deliberately do not override clinit
-
-  public static int valueOf__Z__Ljava_lang_Boolean_2 (MJIEnv env, int clsRef, boolean val) {
-    return env.valueOfBoolean(val);
-  }
-}
