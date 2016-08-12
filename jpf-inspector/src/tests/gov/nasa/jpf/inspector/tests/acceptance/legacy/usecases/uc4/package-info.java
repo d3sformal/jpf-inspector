f32ce4f58a340be27d8fa65ea96556f002756539
Petr Hudeček <petrhudecek2010@gmail.com>
2016-08-12 14:09:23 +0200

    use case cleanup

--- jpf-inspector/src/tests/gov/nasa/jpf/inspector/tests/acceptance/legacy/usecases/uc4/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * The fourth use case attempts to demonstrate the "back_step_transition" command.
- */
-package gov.nasa.jpf.inspector.tests.acceptance.legacy.usecases.uc4;
--- jpf-inspector/src/tests/gov/nasa/jpf/inspector/tests/acceptance/legacy/usecases/uc4new/package-info.java
+++ jpf-inspector/src/tests/gov/nasa/jpf/inspector/tests/acceptance/legacy/usecases/uc4new/package-info.java
@@ -1,4 +1,7 @@
 /**
- * The fourth use case attempts to demonstrate the "back_step_transition" command.
+ * The fourth use case demonstrates the "back_step_transition" command and some other features.
+ *
+ * The use case was changed quite a bit from its original form, because with the original use case, it was hard to find out
+ * what is actually supposed to be printed out. The original usecase may be found in Git history.
  */
 package gov.nasa.jpf.inspector.tests.acceptance.legacy.usecases.uc4new;
